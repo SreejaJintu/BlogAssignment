bf430e179adb4ea1dfbdb3bbb92e2d58ff19c97e
sreeja o s <sreejaosreenivasan@gmail.com>
2025-01-31 22:18:19 +0530

    deploying

--- src/pages/Myblogs.jsx
+++ src/pages/Myblogs.jsx
@@ -9,7 +9,7 @@ const MyBlogs = () => {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/posts/blogs"); // Change the URL to your backend endpoint
+        const response = await axios.get("http://localhost:5000/api/blogs"); 
         setBlogs(response.data);
       } catch (error) {
         console.error("Error fetching blogs:", error);
